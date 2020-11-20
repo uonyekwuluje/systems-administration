cd8b0e1aaf5ba7f219d3a1a611c3457fe8903c5c
uonyekwuluje <uionyekwuluje@gmail.com>
2020-11-19 23:35:52 -0500

    updating admin scripts

---
+++ bash-scripts/tutorial/bash-hw.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#: Title     : Hello World
+#: Data      : 2020-11-19
+#: Author    : "Uchechukwu Onyekwuluje" <email@email.com>
+#: Version   : 1.0
+#: Description  : Testing Hello Worls
+#: Options   : None
+
+printf "%s\n" "Hello, World!"
